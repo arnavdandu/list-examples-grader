c0700bfa45c5d9bd5a8bf024a3a8efffb35390e0
Arnav Dandu <adandu@UCSD.EDU>
2022-11-10 09:02:02 -0800

    added grade.sh

--- grade.sh
+++ grade.sh
@@ -5,3 +5,13 @@ set -e
 rm -rf student-submission
 git clone $1 student-submission
 
+if -f student-submission/ListExamples.java; then
+    cp ./student-submission/ListExamples.java ./
+    javac -cp ".:lib\hamcrest-core-1.3.jar:lib\junit-4.13.2.jar" *.java;
+    if $? == 0; then
+        echo "Compiled successfully"
+    else
+        echo "Compilation error."
+        exit 1
+    java TestListExamples
+
