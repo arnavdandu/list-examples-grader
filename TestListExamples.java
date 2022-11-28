fc0ca395c3c2b473fd60b5f2bcd6627c7f231beb
Arnav S Dandu <cs15lfa22dh@ieng6-203.ucsd.edu>
2022-11-27 22:24:08 -0800

    Updated grade.sh

--- TestListExamples.java
+++ TestListExamples.java
@@ -10,8 +10,22 @@ class EvenLengthChecker implements StringChecker {
 }
 
 public class TestListExamples {
-  @Test
-  public void testFilter() {
+  @Test(timeout = 100)
+  public void testFilter_mirroredList() { 
+    List<String> list = new ArrayList<>();
+    StringChecker elc = new EvenLengthChecker();
+    list.add("");
+    list.add("a");
+    list.add("ab");
+    list.add("aba");
+    list.add("ab");
+    list.add("a");
+    list.add("");
+    assertEquals(ListExamplesCorrect.filter(list, elc), ListExamples.filter(list, elc));
+  }
+
+  @Test(timeout = 100)
+  public void testFilter_notMirrored() {
     List<String> list = new ArrayList<>();
     StringChecker elc = new EvenLengthChecker();
     list.add("");
@@ -22,7 +36,7 @@ public class TestListExamples {
     assertEquals(ListExamplesCorrect.filter(list, elc), ListExamples.filter(list, elc));
   }
 
-  @Test
+  @Test(timeout = 100)
   public void testMerge_duplicate() {
     List<String> list = new ArrayList<>();
     List<String> list2 = new ArrayList<>();
@@ -34,4 +48,50 @@ public class TestListExamples {
     assertEquals(ListExamplesCorrect.merge(list, list), ListExamples.merge(list, list));
   }
 
+  @Test(timeout = 100)
+  public void testMerge_list2Shorter() {
+    List<String> list = new ArrayList<>();
+    List<String> list2 = new ArrayList<>();
+    list.add("");
+    list.add("a");
+    list.add("ab");
+    list.add("abc");
+    list.add("abcd");
+    list2.add("bc");
+    list2.add("A");
+    list2.add("zxc");
+    assertEquals(ListExamplesCorrect.merge(list, list2), ListExamples.merge(list, list2));
+  }
+
+  @Test(timeout = 100)
+  public void testMerge_list1Shorter() {
+    List<String> list = new ArrayList<>();
+    List<String> list2 = new ArrayList<>();
+    list2.add("");
+    list2.add("a");
+    list2.add("ab");
+    list2.add("abc");
+    list2.add("abcd");
+    list.add("bc");
+    list.add("A");
+    list.add("zxc");
+    assertEquals(ListExamplesCorrect.merge(list, list2), ListExamples.merge(list, list2));
+  }
+
+  @Test(timeout = 100)
+  public void testMerge_sameSizeLists() {
+    List<String> list = new ArrayList<>();
+    List<String> list2 = new ArrayList<>();
+    list2.add("");
+    list2.add("a");
+    list2.add("ab");
+    list2.add("abc");
+    list2.add("abcd");
+    list.add("bc");
+    list.add("A");
+    list.add("zxc");
+    list.add("ZZ");
+    list.add("gfcfds");
+    assertEquals(ListExamplesCorrect.merge(list, list2), ListExamples.merge(list, list2));
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,19 +1,40 @@
 # Create your grading script here
 
-set -e
+# set -e
 
 rm -rf student-submission
-git clone $1 student-submission
+rm -f *.class
+
+git clone $1 student-submission > out.txt 2> err.txt 
+
+if [ $? -ne 0 ]; then
+    echo "Could not clone repository at \"$1\". Make sure the link is correct."
+    exit 1
+else
+    echo "Cloning repository at \"$1\"..."	
+fi
 
 if [ -f student-submission/ListExamples.java ]; then
 	cp ./student-submission/ListExamples.java ./
-	javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
-	if [ "$?" == 0 ]; then
-	        echo "Compiled successfully"
-		java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples 
+	javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java > out.txt 2> err.txt
+	if [ $? == 0 ]; then
+	        echo "Compiled successfully!"
 	else
-        	echo "Compilation error."
+        	echo "Compilation error!"
         	exit 1
 	fi
-		
+else
+ 	echo "Couldn't find ListExamples.java. Make sure your files are named correctly and are not nested in another directory."
+	exit 1	
 fi
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > out.txt 2> err.txt
+
+grep -E 'There were |There was |[[:digit:]]\) test|Tests run: ' out.txt
+
+cat out.txt | head -n 2 | tail -n 1 > tests.txt
+
+ERRORS=$(grep -o 'E' tests.txt | wc -l | xargs)
+TOTAL_TESTS=$(grep -o '\.' tests.txt | wc -l | xargs)
+CORRECT=$(( $TOTAL_TESTS - $ERRORS ))
+echo "Grade: $CORRECT / $TOTAL_TESTS" 
